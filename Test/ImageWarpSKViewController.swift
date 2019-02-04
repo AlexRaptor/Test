ee3737ee2ab48da05472585de884b52d52a3f277
Alexander Selivanov <alx_selivanov@icloud.com>
2019-02-04 16:45:48 +0700

    add moving pins

--- Test/ImageWarpSKViewController.swift
+++ Test/ImageWarpSKViewController.swift
@@ -22,6 +22,7 @@ class ImageWarpSKViewController: UIViewController {
     }
 
     var pins: [SKNode] = []
+    var touchedPin: SKNode?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,9 +35,55 @@ class ImageWarpSKViewController: UIViewController {
             scene.scaleMode = .aspectFill
 
             configureScene()
-
             view.presentScene(scene)
+
+            addGestures()
         }
+    }
+
+    private func addGestures() {
+
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self,
+                                                          action: #selector(handlePan(recognizer:)))
+
+        scene.view?.addGestureRecognizer(panGestureRecognizer)
+
+    }
+
+    @objc func handlePan(recognizer: UIPanGestureRecognizer) {
+
+        switch recognizer.state {
+
+        case .began:
+
+            let touchLocation = recognizer.location(in: recognizer.view)
+            let skTouchLocation = CGPoint(x: touchLocation.x, y: recognizer.view!.bounds.height - touchLocation.y)
+            
+            touchedPin = scene.nodes(at: skTouchLocation).filter({ self.pins.contains($0) }).first
+
+        case .ended:
+
+            guard let touchedPin = self.touchedPin else { break }
+
+            let translation = recognizer.translation(in: self.view)
+
+            touchedPin.position = CGPoint(x: touchedPin.position.x + translation.x,
+                                          y: touchedPin.position.y - translation.y)
+            self.touchedPin = nil
+
+        case .changed:
+
+            guard let touchedPin = self.touchedPin else { break }
+            let translation = recognizer.translation(in: self.view)
+
+            touchedPin.position = CGPoint(x: touchedPin.position.x + translation.x,
+                                          y: touchedPin.position.y - translation.y)
+
+        default:
+            break
+        }
+
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
     }
 
     private func configureScene() {
@@ -48,14 +95,15 @@ class ImageWarpSKViewController: UIViewController {
     private func configurePins() {
 
         let topLeftPin = SKShapeNode(circleOfRadius: 10)
+        topLeftPin.name = "topLeftPin"
         topLeftPin.fillColor = .red
-
         topLeftPin.position = CGPoint(x: sprite.position.x - sprite.size.width / 2 - PIN_OFFSET,
                                       y: sprite.position.y + sprite.size.height / 2 + PIN_OFFSET)
         pins.insert(topLeftPin, at: PinTypes.topLeft.rawValue)
         scene.addChild(topLeftPin)
 
         let topRightPin = SKShapeNode(circleOfRadius: 10)
+        topRightPin.name = "topRightPin"
         topRightPin.fillColor = .green
         topRightPin.position = CGPoint(x: sprite.position.x + sprite.size.width / 2 + PIN_OFFSET,
                                        y: sprite.position.y + sprite.size.height / 2 + PIN_OFFSET)
@@ -63,6 +111,7 @@ class ImageWarpSKViewController: UIViewController {
         scene.addChild(topRightPin)
 
         let bottomRightPin = SKShapeNode(circleOfRadius: 10)
+        bottomRightPin.name = "bottomRightPin"
         bottomRightPin.fillColor = .blue
         bottomRightPin.position = CGPoint(x: sprite.position.x + sprite.size.width / 2 + PIN_OFFSET,
                                           y: sprite.position.y - sprite.size.height / 2 - PIN_OFFSET)
@@ -70,6 +119,7 @@ class ImageWarpSKViewController: UIViewController {
         scene.addChild(bottomRightPin)
 
         let bottomLeftPin = SKShapeNode(circleOfRadius: 10)
+        bottomLeftPin.name = "bottomLeftPin"
         bottomLeftPin.fillColor = .yellow
         bottomLeftPin.position = CGPoint(x: sprite.position.x - sprite.size.width / 2 - PIN_OFFSET,
                                          y: sprite.position.y - sprite.size.height / 2 - PIN_OFFSET)
