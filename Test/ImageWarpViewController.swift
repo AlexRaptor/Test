6df4710fa9543d46d2fa9f51c775f6518513c4b8
Alexander Selivanov <alx_selivanov@icloud.com>
2019-02-05 16:06:44 +0700

    update

--- Test/ImageWarpViewController.swift
+++ Test/ImageWarpViewController.swift
@@ -15,19 +15,11 @@ class ImageWarpViewController: UIViewController {
     let SPRITE_SIZE = CGSize(width: 200, height: 200) // swiftlint:disable:this identifier_name
     let PIN_OFFSET: CGFloat = 10 // swiftlint:disable:this identifier_name
 
-//    lazy var sourceSizeX: CGFloat =
-//        (sourcePinPositions[PinTypes.bottomRight.rawValue].x - sourcePinPositions[PinTypes.bottomLeft.rawValue].x)
-//    lazy var sourceSizeY: CGFloat =
-//        (sourcePinPositions[PinTypes.topLeft.rawValue].y - sourcePinPositions[PinTypes.bottomLeft.rawValue].y)
-
     enum PinTypes: Int {
         case bottomLeft, bottomRight, topLeft, topRight
     }
 
     var pins: [UIImageView] = []
-//    var sourcePinPositions: [CGPoint] = []
-//    let sourcePositions: [float2] = [float2(0, 0), float2(1, 0), float2(0, 1), float2(1, 1)]
-//    var destinationPositions: [float2] = [float2(0, 0), float2(1, 0), float2(0, 1), float2(1, 1)]
 
     var touchedPin: UIImageView?
     var indexOfTouchedPin: Int?
@@ -37,86 +29,24 @@ class ImageWarpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        view.backgroundColor = .gray
 
         configureImage(named: "monalisa.jpg")
 
         configurePins()
     }
 
-    private func configureGestures() {
-
-        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
-        view.addGestureRecognizer(panGestureRecognizer)
-    }
-
     @objc func handlePan(recognizer: UIPanGestureRecognizer) {
 
-        print("pan")
+        guard let touchedView = recognizer.view else { return }
 
-        switch recognizer.state {
+        let translation = recognizer.translation(in: recognizer.view)
 
-        case .began:
-            print("began")
-            let touchLocation = recognizer.location(in: recognizer.view)
-//            let skTouchLocation = CGPoint(x: touchLocation.x, y: recognizer.view!.bounds.height - touchLocation.y)
+        touchedView.center += translation
 
-//            guard let touchedPin = view.subviews(at:)
-            nodes(at: skTouchLocation).filter({ self.pins.contains($0) }).first
-                else {
+        recognizer.setTranslation(CGPoint.zero, in: touchedView)
 
-                    print("touchLocation: \(touchLocation)")
-                    print("skTouchLocation: \(skTouchLocation)")
-
-                    for pin in pins {
-                        print("\(pin.name): \(pin.position)")
-                    }
-
-                    break
-
-            }
-
-            self.touchedPin = touchedPin
-            indexOfTouchedPin = pins.firstIndex(of: touchedPin)
-
-            if indexOfTouchedPin == nil {
-                print("NIL")
-            } else {
-                print("indexOfTouchedPin: \(indexOfTouchedPin)")
-            }
-
-        case .ended:
-            print("ended")
-            guard let touchedPin = self.touchedPin else { break }
-
-            let translation = recognizer.translation(in: self.view)
-
-            touchedPin.position = CGPoint(x: touchedPin.position.x + translation.x,
-                                          y: touchedPin.position.y - translation.y)
-            self.touchedPin = nil
-            self.indexOfTouchedPin = nil
-            print("\n\n\n")
-        case .changed:
-            //print("changed")
-            guard let touchedPin = self.touchedPin,
-                let indexOfTouchedPin = self.indexOfTouchedPin
-                else { break }
-
-            let translation = recognizer.translation(in: self.view)
-
-            touchedPin.position = CGPoint(x: touchedPin.position.x + translation.x,
-                                          y: touchedPin.position.y - translation.y)
-
-            recalculateDestinationPositions(indexOfTouchedPin, touchedPin)
-
-            warpImage()
-
-        default:
-            print("default")
-            break
-        }
-
-        recognizer.setTranslation(CGPoint.zero, in: self.view)
+        warpImage()
     }
 
     private func configureImage(named: String) {
@@ -131,43 +61,42 @@ class ImageWarpViewController: UIViewController {
 
         imageView.contentMode = .scaleAspectFill
 
-        imageView.backgroundColor = .gray
+        imageView.layer.borderWidth = 1
+        imageView.layer.borderColor = UIColor.green.cgColor
+
         imageView.image = monaLisa
     }
 
-    @IBAction func testButtonTapped(_ sender: UIButton) {
+    private func warpImage() {
 
         guard let perspectiveTransform = CIFilter(name: "CIPerspectiveTransform")
             else { print("Filter not found"); return }
 
-        let bottomLeft = CGPoint(x: 0, y: 0)
-        let bottomRight = CGPoint(x: 200, y: 0)
-        let topLeft = CGPoint(x: 0, y: 200)
-        let topRight = CGPoint(x: 250, y: 250)
+        let imageBottom = imageView.frame.maxX
+
+        let bottomLeft = pins[PinTypes.bottomLeft.rawValue].center + CGPoint(x: PIN_OFFSET, y: -PIN_OFFSET)
+        let bottomRight = pins[PinTypes.bottomRight.rawValue].center + CGPoint(x: -PIN_OFFSET, y: -PIN_OFFSET)
+        let topLeft = pins[PinTypes.topLeft.rawValue].center + CGPoint(x: PIN_OFFSET, y: PIN_OFFSET)
+        let topRight = pins[PinTypes.topRight.rawValue].center + CGPoint(x: -PIN_OFFSET, y: +PIN_OFFSET)
 
         guard let minMax = getMinAndMaxPoints(points: bottomLeft, bottomRight, topLeft, topRight) else { return }
-
-//        let origin = imageView.frame.origin
 
         let newWidth = minMax.max.x - minMax.min.x
         let newHeight = minMax.max.y - minMax.min.y
 
         let newSize = CGSize(width: newWidth, height: newHeight)
 
-//        print("newSize: \(newWidth), \(newHeight)")
+        imageView.frame = CGRect(origin: minMax.min, size: newSize)
 
-//        print("old origin: \(imageView.frame.origin)")
+        let filterBottomLeft = CGPoint(x: bottomLeft.x, y: imageBottom - bottomLeft.y)
+        let filterBottomRight = CGPoint(x: bottomRight.x, y: imageBottom - bottomRight.y)
+        let filterTopLeft = CGPoint(x: topLeft.x, y: imageBottom - topLeft.y)
+        let filterTopRight = CGPoint(x: topRight.x, y: imageBottom - topRight.y)
 
-        imageView.frame.size = newSize
-//        imageView.frame = CGRect(origin: origin, size: newSize)
-//        imageView.center = center
-
-//        print("new origin: \(imageView.frame.origin)")
-
-        perspectiveTransform.setValue(CIVector(cgPoint: bottomLeft), forKey: "inputBottomLeft")
-        perspectiveTransform.setValue(CIVector(cgPoint: bottomRight), forKey: "inputBottomRight")
-        perspectiveTransform.setValue(CIVector(cgPoint: topLeft), forKey: "inputTopLeft")
-        perspectiveTransform.setValue(CIVector(cgPoint: topRight), forKey: "inputTopRight")
+        perspectiveTransform.setValue(CIVector(cgPoint: filterBottomLeft), forKey: "inputBottomLeft")
+        perspectiveTransform.setValue(CIVector(cgPoint: filterBottomRight), forKey: "inputBottomRight")
+        perspectiveTransform.setValue(CIVector(cgPoint: filterTopLeft), forKey: "inputTopLeft")
+        perspectiveTransform.setValue(CIVector(cgPoint: filterTopRight), forKey: "inputTopRight")
 
         perspectiveTransform.setValue(ciImage, forKey: kCIInputImageKey)
 
@@ -175,7 +104,6 @@ class ImageWarpViewController: UIViewController {
 
         imageView.image = UIImage(ciImage: newImage)
     }
-
     private func getMinAndMaxPoints(points: CGPoint...) -> (min: CGPoint, max: CGPoint)? {
 
         guard let firstPoint = points.first else { return nil }
@@ -194,13 +122,19 @@ class ImageWarpViewController: UIViewController {
         return (min: min, max: max)
     }
 
+    private func configureGestures(target: UIView) {
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
+        target.addGestureRecognizer(panGestureRecognizer)
+    }
+
     private func configurePins() {
 
-//        let pinImage = UIImage(named: "scale")
+        //        let pinImage = UIImage(named: "scale")
 
         let frame = CGRect(x: 0, y: 0, width: 20, height: 20)
 
         var pin = UIImageView(frame: frame)
+        pin.isUserInteractionEnabled = true
         pin.layer.cornerRadius = pin.frame.height / 2
         pin.layer.masksToBounds = true
         pin.contentMode = .scaleAspectFit
@@ -208,8 +142,10 @@ class ImageWarpViewController: UIViewController {
         pin.center = CGPoint(x: imageView.frame.minX - PIN_OFFSET, y: imageView.frame.maxY + PIN_OFFSET)
         pins.insert(pin, at: PinTypes.bottomLeft.rawValue)
         view.addSubview(pin)
+        configureGestures(target: pin)
 
         pin = UIImageView(frame: frame)
+        pin.isUserInteractionEnabled = true
         pin.layer.cornerRadius = pin.frame.height / 2
         pin.layer.masksToBounds = true
         pin.contentMode = .scaleAspectFit
@@ -217,8 +153,10 @@ class ImageWarpViewController: UIViewController {
         pin.center = CGPoint(x: imageView.frame.maxX + PIN_OFFSET, y: imageView.frame.maxY + PIN_OFFSET)
         pins.insert(pin, at: PinTypes.bottomRight.rawValue)
         view.addSubview(pin)
+        configureGestures(target: pin)
 
         pin = UIImageView(frame: frame)
+        pin.isUserInteractionEnabled = true
         pin.layer.cornerRadius = pin.frame.height / 2
         pin.layer.masksToBounds = true
         pin.contentMode = .scaleAspectFit
@@ -226,8 +164,10 @@ class ImageWarpViewController: UIViewController {
         pin.center = CGPoint(x: imageView.frame.minX - PIN_OFFSET, y: imageView.frame.minY - PIN_OFFSET)
         pins.insert(pin, at: PinTypes.topLeft.rawValue)
         view.addSubview(pin)
+        configureGestures(target: pin)
 
         pin = UIImageView(frame: frame)
+        pin.isUserInteractionEnabled = true
         pin.layer.cornerRadius = pin.frame.height / 2
         pin.layer.masksToBounds = true
         pin.contentMode = .scaleAspectFit
@@ -235,5 +175,6 @@ class ImageWarpViewController: UIViewController {
         pin.center = CGPoint(x: imageView.frame.maxX + PIN_OFFSET, y: imageView.frame.minY - PIN_OFFSET)
         pins.insert(pin, at: PinTypes.topRight.rawValue)
         view.addSubview(pin)
+        configureGestures(target: pin)
     }
 }
