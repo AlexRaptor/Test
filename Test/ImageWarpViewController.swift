7ade18f6a0e6b9f76ef57f14c28b31db81d3cb51
Alexander Selivanov <alx_selivanov@icloud.com>
2019-02-05 13:07:54 +0700

    update

--- Test/ImageWarpViewController.swift
+++ Test/ImageWarpViewController.swift
@@ -21,6 +21,12 @@ class ImageWarpViewController: UIViewController {
 
         ciImage = CIImage(cgImage: monaLisa.cgImage!)
 
+        imageView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        imageView.center = imageView.superview?.center
+            ?? CGPoint(x: UIScreen.main.bounds.maxX / 2, y: UIScreen.main.bounds.maxY / 2)
+        imageView.contentMode = .scaleAspectFill
+
+        imageView.backgroundColor = .gray
         imageView.image = monaLisa
     }
 
@@ -29,21 +35,50 @@ class ImageWarpViewController: UIViewController {
         guard let perspectiveTransform = CIFilter(name: "CIPerspectiveTransform")
             else { print("Filter not found"); return }
 
-        let topLeft = CGPoint(x: 0, y: 200)
-        let topRight = CGPoint(x: 150, y: 150)
-        let bottomRight = CGPoint(x: 200, y: 50)
         let bottomLeft = CGPoint(x: 0, y: 0)
+        let bottomRight = CGPoint(x: 200, y: 0)
+        let topLeft = CGPoint(x: 0, y: 200)
+        let topRight = CGPoint(x: 250, y: 250)
 
+        guard let minMax = getMinAndMaxPoints(points: bottomLeft, bottomRight, topLeft, topRight) else { return }
+
+        let center = imageView.center
+
+        let newWidth = minMax.max.x - minMax.min.x
+        let newHeight = minMax.max.y - minMax.min.y
+
+        print("newSize: \(newWidth), \(newHeight)")
+
+        imageView.frame = CGRect(x: 0, y: 0, width: newWidth, height: newHeight)
+        imageView.center = center
+
+        perspectiveTransform.setValue(CIVector(cgPoint: bottomLeft), forKey: "inputBottomLeft")
+        perspectiveTransform.setValue(CIVector(cgPoint: bottomRight), forKey: "inputBottomRight")
         perspectiveTransform.setValue(CIVector(cgPoint: topLeft), forKey: "inputTopLeft")
         perspectiveTransform.setValue(CIVector(cgPoint: topRight), forKey: "inputTopRight")
-        perspectiveTransform.setValue(CIVector(cgPoint: bottomRight), forKey: "inputBottomRight")
-        perspectiveTransform.setValue(CIVector(cgPoint: bottomLeft), forKey: "inputBottomLeft")
 
         perspectiveTransform.setValue(ciImage, forKey: kCIInputImageKey)
 
         guard let newImage = perspectiveTransform.outputImage else { return }
 
         imageView.image = UIImage(ciImage: newImage)
+    }
 
+    private func getMinAndMaxPoints(points: CGPoint...) -> (min: CGPoint, max: CGPoint)? {
+
+        guard let firstPoint = points.first else { return nil }
+
+        var min = firstPoint
+        var max = firstPoint
+        
+        for point in points {
+
+            if point.x < min.x { min.x = point.x }
+            if point.x > max.x { max.x = point.x }
+            if point.y < min.y { min.y = point.y }
+            if point.y > max.y { max.y = point.y }
+        }
+
+        return (min: min, max: max)
     }
 }
