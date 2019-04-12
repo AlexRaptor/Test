e02f7f4c90548fd7ebfcb877046ac1d9552ce4c7
Alexander Selivanov <alx_selivanov@icloud.com>
2019-04-12 17:57:03 +0700

    Add templates for image rotation test

---
+++ Test/ImageRotationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageRotationViewController.swift
+//  Test
+//
+//  Created by Alexander on 12/04/2019.
+//  Copyright © 2019 Alexander Selivanov. All rights reserved.
+//
+
+import UIKit
+
+class ImageRotationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Image Rotation"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
