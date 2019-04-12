6b101a3afa3b17c1bcaa3007d98d951f59e8ffb6
Alexander Selivanov <alx_selivanov@icloud.com>
2019-04-12 18:42:09 +0700

    test in progress

---
+++ Test/Image Rotation/CarouselView.swift
@@ -0,0 +1,26 @@
+//
+//  CarouselView.swift
+//  Test
+//
+//  Created by Alexander on 12/04/2019.
+//  Copyright © 2019 Alexander Selivanov. All rights reserved.
+//
+
+import UIKit
+
+class CarouselView: UIView {
+
+    @IBOutlet weak var leftView: UIView!
+    @IBOutlet weak var rightView: UIView!
+    @IBOutlet weak var centerView: UIView!
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Test/ImageRotationViewController.swift
+++ Test/ImageRotationViewController.swift
@@ -9,22 +9,56 @@
 import UIKit
 
 class ImageRotationViewController: UIViewController {
-
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    
+    private var imagesArray1 = [UIImage?]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         title = "Image Rotation"
+        
+        initializeData()
+        initializeUI()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func initializeData() {
+        imagesArray1 = [
+            UIImage(named: "Color_0"), UIImage(named: "BW_0")
+        ]
     }
-    */
-
+    
+    func initializeUI() {
+        if let firstImage = imagesArray1.first {
+            imageView.image = firstImage
+        }
+    }
+    
+    @IBAction private func onStartTap(_ sender: UIButton) {
+        
+        guard let firstImage = imagesArray1.first else { return }
+        guard let lastImage = imagesArray1.last else { return }
+        
+        sender.isEnabled = false
+        
+        UIView.transition(with: self.imageView,
+                          duration:1,
+                          options: .transitionCrossDissolve,
+                          animations: { [weak self] in
+                            self?.imageView.image = lastImage
+                            
+        }) { (_) in
+            UIView.transition(with: self.imageView,
+                              duration:1,
+                              options: .transitionCrossDissolve,
+                              animations: { [weak self] in
+                                self?.imageView.image = firstImage
+                                
+            }) { (_) in
+                sender.isEnabled = true
+            }
+        }
+    }
 }
